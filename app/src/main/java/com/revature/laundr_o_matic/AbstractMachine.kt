62bd4a6c69544d365ea2c70e975563aa14df16f4
RyanKCox <ryankcox90@gmail.com>
2022-03-15 08:26:19 -0500

    Added basic structure for Machines and a Manager class to hold them

---
+++ app/src/main/java/com/revature/laundr_o_matic/AbstractMachine.kt
@@ -0,0 +1,8 @@
+package com.revature.laundr_o_matic
+
+abstract class AbstractMachine(val id:Int,
+                               var nCost:Float=5.00f,
+                               var nRunTime:Int = 40,
+var nLoadSize:Int = 2) {
+
+}
---
+++ app/src/main/java/com/revature/laundr_o_matic/Washer.kt
@@ -0,0 +1,4 @@
+package com.revature.laundr_o_matic
+
+class Washer(id:Int): AbstractMachine(id) {
+}
