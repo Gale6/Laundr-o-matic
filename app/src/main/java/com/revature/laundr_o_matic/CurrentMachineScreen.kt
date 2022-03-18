1817036c28500672e8aea663529af0836be67ca1
Evan Jones <eljones02@student.ysu.edu>
2022-03-18 16:56:12 -0400

    Fixed Stopwatch

--- app/src/main/java/com/revature/laundr_o_matic/CurrentMachineScreen.kt
+++ app/src/main/java/com/revature/laundr_o_matic/CurrentMachineScreen.kt
@@ -1,15 +1,15 @@
 package com.revature.laundr_o_matic
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.revature.laundr_o_matic.ui.theme.LaundromaticTheme
+import com.revature.laundr_o_matic.utility.Timer
 
 @Composable
 fun CurrentMachineScreen(navController: NavController)
@@ -55,21 +56,9 @@ fun CurrentMachineScreen(navController: NavController)
                         textAlign = TextAlign.Center
                     )
                     Spacer(modifier = Modifier.height(5.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.washing_machine),
-                        contentDescription = "Washer Icon",
-                        Modifier.size(250.dp)
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    LinearProgressIndicator(
-                        progress = 0.7f,
-                        modifier = Modifier
-                            .width(300.dp)
-                            .height(15.dp),
-                        backgroundColor = Color.LightGray,
-                        color = colorResource(id = R.color.customDarkBrown)
-                    )
-                    Spacer(modifier = Modifier.height(7.dp))
+
+                    Timer(3000, 0f)
+
                     Text(
                         text = "You will be notified five minutes\n before your cycle ends.",
                         fontSize = 25.sp,
@@ -97,8 +86,6 @@ fun CurrentMachineScreen(navController: NavController)
             }
         }
     }
-
-
 @Preview (showBackground = true)
 @Composable
 fun CurrentMachineScreenPreview() {
--- app/src/main/java/com/revature/laundr_o_matic/utility/Stopwatch.kt
+++ app/src/main/java/com/revature/laundr_o_matic/utility/Stopwatch.kt
@@ -1,5 +1,6 @@
 package com.revature.laundr_o_matic.utility
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -11,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -22,78 +24,92 @@ import kotlinx.coroutines.delay
 
 
 @Composable
-fun Timer(totalTime: Double, initialValue: Float = 0f) {
+fun Timer(totalTimeInSeconds: Int, initialProgress: Float) {
+    var context = LocalContext.current
 
-    var progress by remember {             //variable for progress
-        mutableStateOf(initialValue)
+    var progress by remember {
+        mutableStateOf(initialProgress)
     }
-    var currentTime by remember {       //variable for the current time
-        mutableStateOf(totalTime)
+    var timeElapsed by remember {
+        mutableStateOf(totalTimeInSeconds)
+    }
+    var timeRemaining by remember {       //variable for the current time
+        mutableStateOf(totalTimeInSeconds)
     }
     var isTimerRunning by remember {    //variable for isTimerRunning
         mutableStateOf(false)
     }
-    LaunchedEffect(key1 = currentTime, key2 = isTimerRunning) {
-        if (currentTime > 0 && isTimerRunning) {
-            delay(100L)
-            currentTime -= 100L
-            progress = (currentTime / totalTime).toFloat()
+
+    LaunchedEffect(key1 = timeRemaining, key2 = isTimerRunning) {
+        if (timeRemaining > 0 && isTimerRunning) {
+            delay(1000L)
+            timeRemaining -= 1
+            timeElapsed = totalTimeInSeconds - timeRemaining
+            progress = (initialProgress + timeElapsed) / totalTimeInSeconds
         }
     }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .clip(RoundedCornerShape(30.dp))
-            .padding(10.dp)
+    val timeMinutes = (timeRemaining / 60)
+    val timeSeconds = (timeRemaining % 60)
+
+
+
+
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(200.dp)
+        .padding(5.dp)
     ) {
-        Text(
-            text = (currentTime / 1000L).toString(),
-            fontSize = 44.sp,
-            color = Color.Black,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier.height(5.dp))
-        Button(     //Button to stop and start the timer
-            onClick = {
-                if (currentTime <= 0L) {
-                    currentTime = totalTime
-                    isTimerRunning = true
-                } else {
-                    isTimerRunning = !isTimerRunning
-                }
-            },
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = if (!isTimerRunning || currentTime <= 0L) {
-                    colorResource(id = R.color.tealGreen)
-                } else {
-                    colorResource(id = R.color.customDarkBrown)
-                }
-            )
-        ) {
-            Text(
-                text = if (isTimerRunning && currentTime >= 0) "Stop"
-                else if (!isTimerRunning && currentTime >= 0L) "Start"
-                else "Restart"
-            )
-        }
-        Spacer(modifier = Modifier.height(5.dp))
-        LinearProgressIndicator(
-            progress = -(progress),
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(15.dp),
-            backgroundColor = Color.LightGray,
-            color = colorResource(id = R.color.customDarkBrown)
-        )
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(30.dp))
+                .padding(10.dp)
+        ) {
+            val timeDisplay = if (timeMinutes < 10 && timeSeconds < 10) {
+                "0$timeMinutes:0$timeSeconds"
+            } else if (timeMinutes < 10) {
+                "0$timeMinutes:$timeSeconds"
+            } else if (timeSeconds < 10) {
+                "$timeMinutes:0$timeSeconds"
+            } else "$timeMinutes:$timeSeconds"
+
+            Text(
+                text = (timeDisplay),
+                fontSize = 44.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            if (!isTimerRunning) {
+                Button(     //Button to stop and start the timer
+                    onClick = { isTimerRunning = true },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.tealGreen))
+                ) {
+                    Text(
+                        text = "Start"
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+            LinearProgressIndicator(
+                progress = progress,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(15.dp),
+                backgroundColor = Color.LightGray,
+                color = colorResource(id = R.color.customDarkBrown)
+            )
+            val percentCompleted = (progress * 100).toInt()
+            Text(text = "$percentCompleted% complete.")
+        }
     }
 }
-
 @Preview
 @Composable
 fun PreviewTimer() {
-    Timer(50000.00)
+    Timer(3000,0f)
 }
